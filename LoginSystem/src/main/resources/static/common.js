fa9532a6affb56ccc7e84e25b7c76ddaffdaaa20
itg <itg@DESKTOP-JF8J5O5>
2025-01-23 14:54:13 +0900

    fin_comment

--- LoginSystem/src/main/resources/static/common.js
+++ LoginSystem/src/main/resources/static/common.js
@@ -1,13 +1,22 @@
-async function login(mid,mpw){ //로그인 함수 분리 (해당 함수는 아이디,비밀번호를 넣어주면 리턴해줌)
-	const logindata = {mid:$("#mid").val(),mpw:$("#mpw").val()}
-	//json은 따로 헤더값을 줘야함 --> "content-type":"application/json" -규정되어있음
+async function login(mid,mpw){//로그인 함수 분리
+	const logindata = {mid,mpw}
 	const ctype = "application/json"
-	const res = await fetch("/member/login",{method:"post",headers:{"content-type":ctype},body:JSON.stringify(logindata)})
+	const res = await fetch("/member/login",{method:"post",headers:{"Content-type":ctype},body:JSON.stringify(logindata)})
 	const data = await res.json();
 	return data;
 }
-function logout(logoutui){ //로그아웃 함수 분리
-	logoutui.attr("href","/member/logout").text("로그아웃") //체이닝 /setter
+function logout(logoutui){//로그아웃 함수분리
+	logoutui.attr("href","/member/logout").text("로그아웃")
+	logoutui.click(async function(evt){
+				if($(this).text()=="로그아웃"){
+					evt.preventDefault();
+					const res = await fetch($(this).attr("href"))
+					const data = await res.json();
+					alert(data.message)
+					location.href="/";
+					return false;
+				}
+	})
 }
 
 
--- dataset.js
+++ dataset.js
@@ -26,29 +26,32 @@ class DataSet{
 
 let d1 = new DataSet("개발환경구축")//메인 타이틀 //메뉴번호
 d1.set_content("사용자의 로그인 기능을 구현하여 로그인 사용자와 비로그인 사용자에 제공 서비스를 구분한다.")//서브 타이틀
-d1.set_img(0,{imgtitle:"요구사항명세서작성",imgurl:"https://drive.google.com/file/d/1rg-utubawL4ex1iDmxPa_mZ5LCc_kAFJ/view?usp=drive_link",imglog:"작성된 요구사항명세서",sourceurl:"https://docs.google.com/document/d/1YQG5y8IdsTug-SbBu_nv7jKVqG1j9O_gBv_HOyBOxNY/edit?usp=drive_link"})//이미지타이틀
+d1.set_img(0,{imgtitle:"요구사항명세서작성",imgurl:"https://drive.google.com/file/d/19IQoXiARr83tIhM0q3mQAfyLWCxvOPw-/view?usp=drive_link",imglog:"작성된 요구사항명세서",sourceurl:"https://docs.google.com/document/d/1YQG5y8IdsTug-SbBu_nv7jKVqG1j9O_gBv_HOyBOxNY/edit?usp=drive_link"})//이미지타이틀
 
 
 d1.set_content("목표 시스템 개발을 위한 적정한 언어를 선정하고 라이선스와 향후 지원사항을 참고하여 결정한다.")
-d1.set_img(1,{imgtitle:"개발언어선정",imgurl:"https://drive.google.com/file/d/1z4mFDvovnvQnXVqeEbAWOa-ja-JU2dJz/view?usp=drive_link",imglog:"자바언어기반 스프링프레임워크, html5, memberData file",sourceurl:"https://docs.google.com/document/d/1BKwroidze5UCn-ou01jyU_de8V2fvyDsgBSCfHY4KZE/edit?usp=drive_link"})//이미지타이틀
+d1.set_img(1,{imgtitle:"개발언어선정",imgurl:"https://drive.google.com/file/d/1z4mFDvovnvQnXVqeEbAWOa-ja-JU2dJz/view?usp=drive_link",imglog:"자바언어기반 스프링프레임워크, html5, memberData file",sourceurl:"https://docs.google.com/document/d/1YpiEmIeDize5ZMk2rHEjsXvn0ATa6j9UuLskoi_dPyM/edit?usp=sharing"})//이미지타이틀
 d1.set_img(1,{imgtitle:"클라이언트 개발언어",imgurl:"https://drive.google.com/file/d/1GWZaucsIKEO962jIj0BVQ1syBfk2QUG1/view?usp=drive_link",imglog:"작성문서_html5,css,javascript",sourceurl:""})//이미지타이틀
 d1.set_img(1,{imgtitle:"데이터베이스세팅",imgurl:"https://drive.google.com/file/d/1z26KQSTE7Y7ObTzmmaPKtpuAevO-8cV0/view?usp=drive_link",imglog:"마리아DB설정 및 데이베이스생성",sourceurl:""})
 
 d1.set_content("코딩개발도구와 테스트 및 빌드, 패키징, 배포등 개발 전반에 대한 필요한 환경을 설정한다.")
-d1.set_img(2,{imgtitle:"개발환경 구축 명세",imgurl:"https://drive.google.com/file/d/1mw6ZBtTbbZQYlfz1YQXCuTfoi-hnvfpg/view?usp=drive_link",imglog:"운영체제 및 개발에 필요한 환경을 고려하여 개발도구, 테스트도구, 빌드도구, 패키징도구, 형상관리도구등을 결정",sourceurl:"https://docs.google.com/document/d/1rNVbHnhYGoozaqX_RsD3cErmKYzGUMvba9ihxbP2CKk/edit?usp=drive_link"})//이미지타이틀
+d1.set_img(2,{imgtitle:"개발환경 구축 명세",imgurl:"https://drive.google.com/file/d/1Gll6QttUHKmhFUiOl_U5IpEBosmC0kCX/view?usp=drive_link",imglog:"운영체제 및 개발에 필요한 환경을 고려하여 개발도구, 테스트도구, 빌드도구, 패키징도구, 형상관리도구등을 결정",sourceurl:"https://docs.google.com/document/d/1rNVbHnhYGoozaqX_RsD3cErmKYzGUMvba9ihxbP2CKk/edit?usp=drive_link"})//이미지타이틀
+d1.set_img(2,{imgtitle:"개발도구 모음",imgurl:"https://drive.google.com/file/d/1WUGAYWtoG_xuOQTKqMuP97oKoAWUeoHz/view?usp=drive_link",imglog:"서버 개발을 효율적으로 하고자 도구를 모아둠",sourceurl:""})//이미지타이틀
 d1.set_img(2,{imgtitle:"개발도구 Spring for Eclipse",imgurl:"https://drive.google.com/file/d/1vl3Ridea05eDn3Hx0cfIWzgkPi-vQX31/view?usp=drive_link",imglog:"자바 스프링 서버단 개발을 위한 도구",sourceurl:""})//이미지타이틀
 d1.set_img(2,{imgtitle:"빌드도구 gradle",imgurl:"https://drive.google.com/file/d/1vlxyuzBmzaBk7B9VoycUH0mRBBxe4nTr/view?usp=drive_link",imglog:"프로그램 빌드 및 외부 라이브러리 디펜더시를 위한 도구",sourceurl:""})//이미지타이틀
-d1.set_img(2,{imgtitle:"형상관리도구 git",imgurl:"https://drive.google.com/file/d/1yLXiqHWblzxAnZXGZKvRcxU8HSgf3UqS/view?usp=drive_link",imglog:"프로그램의 전반적인 개발협업 및 버전관를 위한 도구",sourceurl:""})//이미지타이틀
+d1.set_img(2,{imgtitle:"형상관리도구 git",imgurl:"https://drive.google.com/file/d/1XrUo9XG8NQySh6ljHQ4ECJ21XZGZ3d4p/view?usp=drive_link",imglog:"프로그램의 전반적인 개발협업 및 버전관를 위한 도구",sourceurl:""})//이미지타이틀
 
 d1.set_fill("서버의 보안성과 각 수행기능별 패턴을 분리하기 위해 Spring WAS 와 사용자 View 기능을 위해 웹브라우져에서 일반적인 작동이 가능한 HTML5 를 구현하며 데이터베이스 연동 대신 파일로 회원목록 저장")//사용자 에필로그
 data_sets.push(d1)
 
 // menu2 =============================================================
 let d2 = new DataSet("공통모듈구현")//메인타이틀
-
 d2.set_content("공통모듈구현을 작성합니다.")//서브 타이틀
+d2.set_img(0,{imgtitle:"공통모듈구현을 위한 파일생성",imgurl:"https://drive.google.com/file/d/1EYb6PrdWkTxfPPKB8KjUJ7xbZevExASu/view?usp=drive_link",imglog:"common.js파일을 생성하여 공통모듈(로그아웃,로그인)을 이곳에 구현하였다. 함수만 호출하면 어디에서든 로그인,로그아웃 기능을 사용할 수 있다. ",sourceurl:"https://docs.google.com/document/d/18yo1TRTYfw4Drvh53a1N_uzvgxQvWYf1O2lY5I8hwWc/edit?usp=drive_link"})//이미지타이틀
 d2.set_content("테스트케이스를 작성합니다.")
+d2.set_img(1,{imgtitle:"구현한 공통모듈 작동확인",imgurl:"https://drive.google.com/file/d/1EYb6PrdWkTxfPPKB8KjUJ7xbZevExASu/view?usp=drive_link",imglog:"common.js파일을 생성하여 공통모듈(로그아웃,로그인)을 이곳에 구현하였다. 함수만 호출하면 어디에서든 로그인,로그아웃 기능을 사용할 수 있다. ",sourceurl:""})//이미지타이틀
 d2.set_content("단위테스트를 작성합니다.")
+d2.set_img(2,{imgtitle:"공통모듈구현하기",imgurl:"https://drive.google.com/file/d/1EYb6PrdWkTxfPPKB8KjUJ7xbZevExASu/view?usp=drive_link",imglog:"common.js파일을 생성하여 공통모듈(로그아웃,로그인)을 이곳에 구현하였다. 함수만 호출하면 어디에서든 로그인,로그아웃 기능을 사용할 수 있다. ",sourceurl:""})//이미지타이틀
 
 d2.set_fill("사용자와 프로그램간에 인터렉티브한 로그인 세션 설정")//사용자 에필로그
 data_sets.push(d2)
